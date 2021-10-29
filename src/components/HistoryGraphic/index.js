0f87556a09af729e79b9c5734777aed18174d051
Breno <bhfs.2908@gmail.com>
2021-10-29 17:23:05 -0300

    feat:add components

---
+++ src/components/HistoryGraphic/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import { View, Text } from "react-native";
+
+import { styles } from "./styles";
+
+export function HistoryGraphic() {
+  return <View style={styles.contentGraphic}></View>;
+}
---
+++ src/components/QuotationsList/styles.js
@@ -0,0 +1,23 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  filters: {
+    width: "100%",
+    flexDirection: "row",
+    paddingVertical: 15,
+    justifyContent: "space-evenly",
+  },
+  buttonQuery: {
+    width: 50,
+    height: 30,
+    backgroundColor: "#ff9416",
+    borderRadius: 50,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  textButtonQuery: {
+    color: "#232323",
+    fontWeight: "bold",
+    fontSize: 14,
+  },
+});
