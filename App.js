ea7118ddc94c9f3c7dd6c19bc87ee6e0e52aa6fb
Breno <bhfs.2908@gmail.com>
2021-10-29 19:25:20 -0300

    feat:finish

--- App.js
+++ App.js
@@ -1,18 +1,89 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, View, SafeAreaView, StatusBar } from "react-native";
 import { CurrentPrice } from "./src/components/CurrentPrice";
 import { HistoryGraphic } from "./src/components/HistoryGraphic";
 import { QuotationsList } from "./src/components/QuotationsList";
-import { QuotationsItem } from "./src/components/QuotationsList/QuotationsItem";
+
+function addZero(number) {
+  if (number <= 9) {
+    return "0" + number;
+  }
+
+  return number;
+}
+
+function url(qtdDays) {
+  const date = new Date();
+
+  const listLastDays = qtdDays;
+
+  const end_Date = `${date.getFullYear()}-${addZero(date.getMonth() + 1)}-${addZero(date.getDate())}`;
+  date.setDate(date.getDate() - listLastDays);
+
+  const start_Date = `${date.getFullYear()}-${addZero(date.getMonth() + 1)}-${addZero(date.getDate())}`;
+
+  return `https:api.coindesk.com/v1/bpi/historical/close.json?start=${start_Date}&end=${end_Date}`;
+}
+
+async function getListCoins(url) {
+  let response = await fetch(url);
+  let returnApi = await response.json();
+  let selectListQuotations = returnApi.bpi;
+  const queryCoinList = Object.keys(selectListQuotations).map((key) => {
+    return {
+      data: key.split("-").reverse().join("/"),
+      valor: selectListQuotations[key],
+    };
+  });
+
+  let data = queryCoinList.reverse();
+
+  return data;
+}
+
+async function getPriceCoinsGraphic(url) {
+  let responseG = await fetch(url);
+  let returnApiG = await responseG.json();
+  let selectListQuotationsG = returnApiG.bpi;
+  const queryCoinList = Object.keys(selectListQuotationsG).map((key) => {
+    return selectListQuotationsG[key];
+  });
+
+  let dataG = queryCoinList;
+
+  return dataG;
+}
 
 export default function App() {
+  const [coinsList, setCoinsList] = useState([]);
+  const [coinsGraphicList, setCoinsGraphicList] = useState([0]);
+  const [days, setDays] = useState(30);
+  const [updateData, setUpdateData] = useState(true);
+
+  function updateDay(number) {
+    setDays(number);
+    setUpdateData(true);
+  }
+
+  useEffect(() => {
+    getListCoins(url(days)).then((data) => {
+      setCoinsList(data);
+    });
+
+    getPriceCoinsGraphic(url(days)).then((dataG) => {
+      setCoinsGraphicList(dataG);
+    });
+    if (updateData) {
+      setUpdateData(false);
+    }
+  }, [updateData]);
+
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar backgroundColor="#ff9416" barStyle="dark-content" />
-      <CurrentPrice />
-      <HistoryGraphic />
-      <QuotationsList />
-      <QuotationsItem />
+      <CurrentPrice last={coinsGraphicList[coinsGraphicList.length - 1]} />
+      <HistoryGraphic infoDataGraphic={coinsGraphicList} />
+      <QuotationsList filterDay={updateDay} listTransactions={coinsList} />
     </SafeAreaView>
   );
 }
--- src/components/HistoryGraphic/index.js
+++ src/components/HistoryGraphic/index.js
@@ -1,9 +1,36 @@
 import React from "react";
 
-import { View, Text } from "react-native";
+import { View, Dimensions } from "react-native";
+import { LineChart } from "react-native-chart-kit";
 
 import { styles } from "./styles";
 
-export function HistoryGraphic() {
-  return <View style={styles.contentGraphic}></View>;
+export function HistoryGraphic(props) {
+  return (
+    <View>
+      <LineChart
+        data={{ datasets: [{ data: props.infoDataGraphic }] }}
+        width={Dimensions.get("window").width}
+        height={220}
+        yAxisLabel="$"
+        yAxisSuffix="k"
+        withVerticalLines={false}
+        yLabelsOffset={1}
+        withVerticalLabels={false}
+        chartConfig={{
+          backgroundColor: "#000",
+          backgroundGradientForm: "#232323",
+          backgroundGradientTo: "#3f3f3f",
+          decimalPlaces: 0,
+          color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+          labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+          propsForDots: {
+            r: "1",
+            strokeWidth: "1",
+            stroke: "#ff9416",
+          },
+        }}
+      />
+    </View>
+  );
 }
